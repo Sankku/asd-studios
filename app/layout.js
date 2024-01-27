b38bd2e36fd9071608e209d18ad218f571c76874
Santiago Alaniz <santiagoemprendedor9@gmail.com>
2024-01-26 22:29:43 -0300

    Vercel speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
@@ -6,7 +7,10 @@ const RootLayout = ({ children }) => (
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </head>
-    <body>{children}</body>
+    <body>
+      {children}
+      <SpeedInsights />
+    </body>
   </html>
 );
 
